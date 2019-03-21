11ed5be67008fd86a3a0fe76cca885d0237c450c
Your Name <simeonm@wit.edu>
2019-03-21 12:12:17 -0400

    new

---
+++ T_Avatar/Assets/TreeGrow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeGrow : MonoBehaviour {
+
+    public float maxGrowth = 1;
+    public float speedGrowth = 0.0001f;
+    Vector3 tempScale;
+    public Tree TestTreeObject;
+    public static bool ready = false;
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        tempScale = transform.localScale;
+        if(transform.localScale.x < maxGrowth)
+        {
+            tempScale.x += speedGrowth;
+            tempScale.y  += speedGrowth * 2;
+            tempScale.z += speedGrowth;
+            transform.localScale = tempScale;
+        }
+        else
+        {
+            ready = true;
+        }
+	}
+
+
+}
+
+
---
+++ T_Avatar/Assets/leaves.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leaves : MonoBehaviour {
+
+    public float maxGrowth = 1;
+    public float speedGrowth = 0.0001f;
+    Vector3 tempScale;
+    public Tree TestTreeObject;
+    public GameObject targetObject;
+
+    private bool hideObject;
+    // Use this for initialization
+
+    void Start()
+    {
+        targetObject.SetActive(false);
+        StartCoroutine(Example());
+    }
+
+    IEnumerator Example()
+    {
+      
+        yield return new WaitForSecondsRealtime(20);
+        targetObject.SetActive(true);
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        tempScale = transform.localScale;
+        if (transform.localScale.x < maxGrowth)
+        {
+            tempScale.x += speedGrowth;
+            tempScale.y += speedGrowth * 2;
+            tempScale.z += speedGrowth;
+            transform.localScale = tempScale;
+        }
+
+
+      
+    }
+}
